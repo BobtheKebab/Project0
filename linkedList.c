f0153e01848a6a2080037490034c7c28b5ec8e3f
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-10-17 14:56:19 -0400

    Pasted code from last hw

--- linkedList.c
+++ linkedList.c
@@ -16,4 +16,27 @@ struct song_node * randSong (struct song_node *list);
 struct song_node * removeNode (struct song_node *node);
 struct song_node * freeList (struct song_node *list);
 
+void printList (struct node *list) {
+  int count = 0;
+  while (list) {
+    printf("%d : %d\n", count++, (*list).num);
+    list = (*list).next;
+  }
+}
 
+struct node * insertFront (struct node *list, int data) {
+  struct node *insert = (struct node *) malloc(sizeof(struct node));
+  (*insert).num = data;
+  (*insert).next = list;
+  return insert;
+}
+
+struct node * freeList (struct node *list) {
+  struct node *front = list;
+  while (list) {
+    struct node *remove = list;
+    list = (*list).next;
+    free(remove);
+  }
+  return front;
+}
