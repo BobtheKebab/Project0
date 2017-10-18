d1198453194bc03aa15c3f8e657e8dda9bf8895f
TakingTheL <yliu6@stuy.edu>
2017-10-18 15:27:12 -0400

    search song and add song done

--- leo.c
+++ leo.c
@@ -12,15 +12,31 @@ int charToNum(char in){
   char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
   int counter = 0;
-  //stop when: counter == 26, letter found
+  //stop when: counter == 26 or letter found
   while(in != ALPHABET[counter] && in != alphabet[counter] && counter != 26){
     counter ++;
   }
   return counter;
 }
 
-struct node * searchSong(struct song_node[] musicLib, char[] artist, char[] song){
+struct node * addSong(struct song_node[] musicLib, char[] song, char[] artist){
   int tableNum = charToNum( artist[0]);
   struct song_node* list = musicLib[tableNum];
   insertNode(list, song, artist);
+}
+
+struct node * searchSong(struct song_node[] muiscLib, char[] song, char[] artist){
+  int tableNum = charToNum(artist[0]);
+  struct song_node* list = musicLib[tableNum];
+  //search the song
+  //while the name doesn't match and there is a next song...
+  while((*list).name != song && (*list).next){
+    //go onto the next
+    list = (*list).next;
+  }
+  if ((*list).name == song){
+    return list;
+  } else {
+    return null;
+  }
 }
