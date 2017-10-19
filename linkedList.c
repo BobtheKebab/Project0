ed1c95534b9063d3c4fe5c52dd0ec331bf606d6e
BobtheKebab <ahbab.ashraf@gmail.com>
2017-10-18 21:30:49 -0400

    insertFront works, insertNode still needs work

--- linkedList.c
+++ linkedList.c
@@ -8,7 +8,7 @@ struct song_node{
   struct song_node *next;
 };
 
-struct song_node * insertFront (struct song_node *list, char, char);
+struct song_node * insertFront (struct song_node *list, char[], char[]);
 struct song_node * insertNode (struct song_node *list, char *, char *);
 void printList (struct song_node *list);
 struct song_node * searchSong (struct song_node *list, char *, char *);
@@ -30,16 +30,17 @@ void printList (struct song_node *list) {
 
 struct song_node * insertFront (struct song_node *list, char nombre[], char arte[]) {
   struct song_node *insert = (struct song_node *) malloc(sizeof(struct song_node));
-  (*insert).name = nombre;
-  (*insert).artist = arte;
-  (*insert).next = list;
+  strcpy(insert->name, nombre);
+  strcpy(insert->artist, arte);
+  insert->next = list;
   return insert;
 }
 
-struct song_node * insertNode (struct song_node *list, char nombre[], char arte[]) {
+struct song_node * insertNode (struct song_node *list, char *nombre, char *arte) {
+  struct song_node *front = list;
   struct song_node *insert = (struct song_node *) malloc(sizeof(struct song_node));
-  (*insert).name = nombre;
-  (*insert).artist = arte;
+  strcpy(insert->name, nombre);
+  strcpy(insert->artist, arte);
   // Find node that the insert is after
   while(list) {
     if (strcmp(list->name, insert->name) > 0) break;
@@ -47,6 +48,7 @@ struct song_node * insertNode (struct song_node *list, char nombre[], char arte[
   struct song_node *after = list->next;
   list->next = insert;
   insert->next = after;
+  return front;
 }
 
 struct song_node * freeList (struct song_node *list) {
@@ -58,4 +60,14 @@ struct song_node * freeList (struct song_node *list) {
     free(remove);
   }
   return front;
+}
+
+int main () {
+
+  struct song_node *list = 0;
+  list = insertFront(list, "Slippery", "Migos");
+  //list = insertFront(list, "All Me", "Drake");
+  printList(insertNode(list, "Sacrifices", "Big Sean"));
+  
+  return 0;
 }
